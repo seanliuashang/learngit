ea6ece0b3235bd2022b3c1f89357cba7f8a7c9dc
seanliuashang <lyxhahaha@163.com>
2020-02-18 02:10:45 +0000

    modify python's file:hello.py and create file exercise-for.py

---
+++ exercise-for.py
@@ -0,0 +1,5 @@
+# _*_ coding:utf-8 _*_
+l=["Bart","Lisa","Adam"]
+for n in l:
+    print("hello,"+n)
+    
--- hello.py
+++ hello.py
@@ -16,3 +16,17 @@ print(s4)
 print(n)
 print(f)
 print(s1)
+age = 18
+if age > 20:
+    print('your age is',age)
+    print('adult')
+else:
+    print('your age is',age)
+    print('teanager')
+names=["Michael","Bob","Tracy"]
+for name in names:
+    print(name)
+sum = 0
+for x in range(101):
+    sum=sum+x
+print(sum)    
