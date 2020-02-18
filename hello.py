ffa254e879fc5ff09fb086564bcae8bda5c71b4e
seanliuashang <lyxhahaha@163.com>
2020-02-18 00:23:40 +0000

    add file hello.py

---
+++ hello.py
@@ -0,0 +1,18 @@
+#NNNNNNNNNNNNN
+#-*- coding:utf-8 -*-
+n=123
+print(n)
+f=456.789
+print(f)
+s1="Hello,world"
+print(s1)
+s2="Hello,\'Adam\'"
+print(s2)
+s3="Hello,'Bart'"
+print(s3)
+s4='''"Hello,
+Lisa!"'''
+print(s4)
+print(n)
+print(f)
+print(s1)
